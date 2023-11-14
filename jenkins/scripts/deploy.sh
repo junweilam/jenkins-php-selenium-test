c398bb4a325305a6ad87d45d3ebabd2f459972bb
junweilam <91267859+junweilam@users.noreply.github.com>
2023-11-14 11:10:15 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
+chmod +x "$0"
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test:/var/www/html php:7.2-apache
 sleep 1 
 set +x
