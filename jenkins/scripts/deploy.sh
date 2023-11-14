251873ef0989e7c3f89a1e449853d4ebe507f5d5
junweilam <91267859+junweilam@users.noreply.github.com>
2023-11-14 10:44:06 +0800

    test jenkins

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test:/var/www/html php:7.2-apache
-sleep 1
+sleep 1 
 set +x
 
 echo 'Now...'
